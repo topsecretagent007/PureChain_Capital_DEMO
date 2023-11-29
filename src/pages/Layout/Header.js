a3bd3951408c25a33f17f4b80c8e984120ecf255
lendon1114 <lendonbracewell1114@gmail.com>
2023-11-29 22:54:10 +0900

    updata ui-6

--- src/pages/Layout/Header.js
+++ src/pages/Layout/Header.js
@@ -12,7 +12,7 @@ export default function Header () {
 
     return (
         <div className="w-full h-full justify-center items-center fiexd felx-col bg-black ">
-            <div className="w-full md:w-2/3 lg:w-1/2">
+            <div className="w-full md:w-2/3 lg:w-1/2 mx-auto">
                 <div className="text-white font-semibold text-[16px] items-center justify-between flex felx-col p-3">
                     {
                         MenuList.map((item, index) => {
