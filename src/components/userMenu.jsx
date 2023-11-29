94b749054b112424cbc903e5724648cb336fc169
lendon1114 <lendonbracewell1114@gmail.com>
2023-11-29 22:25:27 +0900

    updata UI

--- src/components/userMenu.jsx
+++ src/components/userMenu.jsx
@@ -17,7 +17,7 @@ export default function UserMenu() {
   ];
   return (
     <div className="w-full h-[calc(100%-96px)] flex flex-col gap-2">
-      <div className="w-full flex flex-row items-center justify-start text-white font-semibold gap-2">
+      <div className="w-full flex flex-row items-center justify-start text-white font-semibold gap-2 text-[16px]">
         {UserMenu.map((item, index) => {
           return (
             <div key={index} className="cursor-pointer">{item.text} ({item.data.length})</div>
